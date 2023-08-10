72e232769fd822e344ed6dc471de94e27baa0102
Alex Tsai <alex.tsai45@gmail.com>
2023-08-11 03:28:47 +0800

    minor functionality

--- flaskr/app.py
+++ flaskr/app.py
@@ -15,12 +15,9 @@ from sqlalchemy import create_engine, func, text
 #################################################
 engine = create_engine('sqlite://', echo = False)
 
+Plastic_df = pd.read_excel("static/assets/Plastic Data.xlsx")
 
-Plastic_df = pd.read_excel("assets/Plastic Data.xlsx")
-
-Plastic_df.to_sql('oceanplastic', con=engine)
-with engine.connect() as conn:
-    print(conn.execute(text("SELECT * FROM oceanplastic")).fetchall())
+Plastic_df.to_sql('oceanplastic', con=engine, if_exists="replace", index=False)
 
 #################################################
 # Flask Setup
@@ -32,11 +29,66 @@ app = Flask(__name__)
 #################################################
 @app.route("/")
 def home():
+    # Default Route: Basic EDA and Map display
     return render_template("home.html")
 
-@app.route("/test")
+# Group By:
+    # Continent
+    # Habitat Type
+    # Water Depth
+
+
+# This section retrieves data 
+@app.route("/api/init")
 def test():
-    return jsonify({"apple":4, "bana":5, "crab":6})
+    # Gotta recreate your engine every time
+    Plastic_df.to_sql('oceanplastic', con=engine, if_exists="replace", index=False)
+    with engine.connect() as conn:
+        results = conn.execute("SELECT * FROM oceanplastic").fetchall()
+
+    # Set up a list of GeoJSON points to push
+    result_list = []
+    for row in results:
+        # Base GeoJSON Template
+        feature = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    # coordinates go here
+                },
+                "properties": {
+                    # everything else goes here
+                }
+            }
+        
+        denominator = row['Corrected_Fibers (n kg-1)']+row['Corrected_Non-Fibrous (n kg-1)']
+
+        if (denominator!=0):
+        # Fill in the rest of the desired details here
+            feature["geometry"]["coordinates"]=[row['Longitude €'], row['Latitude (N)']]
+            feature["properties"]["Area"]=row['Continent']
+            feature["properties"]["Biome"]=row['Habitat_Type']
+            feature["properties"]["Depth"]=row['Water_Depth (m)']
+            feature["properties"]["Population"]=row['Mean Population in 110 km radius (no. people km-2)']
+            feature["properties"]["Fiber_Abundance"]=row['Corrected_Fibers (n kg-1)']
+            feature["properties"]["NonFiber_Abundance"]=row['Corrected_Non-Fibrous (n kg-1)']
+            feature["properties"]["Total_Abundance"]=denominator
+            feature["properties"]["Prop_Fiber"]=row['Corrected_Fibers (n kg-1)']/denominator
+            feature["properties"]["Prop_NonFiber"]=row['Corrected_Non-Fibrous (n kg-1)']/denominator
+
+            # Append completed GeoJSON to list
+            result_list.append(feature)
+        
+    return jsonify(result_list)
+    
+@app.route("/api/agg")
+def agg():
+    # Gotta recreate your engine every time
+    Plastic_df.to_sql('oceanplastic', con=engine, if_exists="replace", index=False)
+    with engine.connect() as conn:
+        results = conn.execute("SELECT * FROM oceanplastic GROUP BY Continent ORDER BY COUNT(ID) DESC").fetchall()
+    
+    return results
 
 @app.errorhandler(404)
 def page_not_found(error):
--- flaskr/static/js/logic.js
+++ flaskr/static/js/logic.js
@@ -1,51 +1,14 @@
+// Adding tilesets here
 let street = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 });
 
-groupA = [];
-groupB = [];
-
-function MarkerOptions(group){
-    if(group=="A"){
-        color="red";
-    }else{
-        color="blue";
-    }
-    return {
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8,
-        color:color
-    };
-} 
-
-for(let i=0; i<5; i++){
-    groupA.push(L.circleMarker([51.505, -0.09+(i*0.02)], MarkerOptions("A")).bindPopup(`Marker A${i+1}`));
-    groupB.push(L.circleMarker([51.505+(i*0.02), -0.11], MarkerOptions("B")).bindPopup(`Marker B${i+1}`))
-}
-
-let A = L.layerGroup(groupA);
-let B = L.layerGroup(groupB);
-
-let baseMaps={
-    "Street Map":street
-};
-
-let overlayMaps={
-    "Group A":A,
-    "Group B":B
-};
-
-let map = L.map('map',{
-    center: [51.505, -0.09],
-    zoom: 11,
-    layers: [street, A, B]
+let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
-d3.json("/test")
 
-let layerControl = L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(map);
 
 let x = [1,2,3,4,5];
 let y = [5,4,3,2,1];
@@ -62,12 +25,131 @@ yaxis:{
     title:"TestY"
 }
 };
+let config = {responsive: true}
 
-Plotly.newPlot("plot", plotData, plotLayout);
+Plotly.newPlot("plot", plotData, plotLayout, config);
+Plotly.newPlot("plot2", plotData, plotLayout, config);
+Plotly.newPlot("plot3", plotData, plotLayout, config);
 
-d3.json("/test").then(function(data){
-    console.log(data);
-    for (const [key, value] of Object.entries(data)) {
-        console.log(key, value);
-      }
+// Define variables and map components globally so we can remove and replace them later
+let plastic, agg, baseMaps, overlayMaps, map, layerControl;
+
+// Define map creation functions here:
+// Looks hacky, but that's because I otherwise don't know how to update maps and their layers
+function defaultMap(data){
+    let geojsonMarkerOptions = {
+        radius: 4,
+        fillColor: "#ff7800",
+        color: "#fff",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    let trash = L.geoJson(data,{
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        },
+
+        // Binding a popup to each layer
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(
+            `<b>Place Occurred:
+            <hr>
+            <b>Latitude:</b> ${feature.geometry.coordinates[1]}<br>
+            <b>Longitude:</b> ${feature.geometry.coordinates[0]}<br>`
+            )
+        }
+    });
+    
+    // Create a baseMaps object.
+    baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo
+    };
+
+    // Create an overlay object to hold our overlay.
+    overlayMaps = {
+        //"Trash": trash
+    };
+
+    // Defining what layers to load in on default
+    map = L.map('map',{
+        center: [0, 0],
+        zoom: 2,
+        layers: [street, trash]
+    });
+    
+    layerControl = L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(map);
+}
+
+
+// Initialize Map and bring data in
+d3.json("/api/init").then(function(data){
+    plastic = data;
+    console.log(plastic);
+
+    defaultMap(data);
 });
+
+d3.json("/api/agg").then(function(data){
+    agg = data;
+});
+
+
+function optionChanged(value){
+    console.log(value);
+    if (value==1){
+        map.off();
+        map.remove();
+        layerControl.remove();
+        mapMaker(plastic);
+    }
+    if (value==5){
+        map.off();
+        map.remove();
+
+        groupA = [];
+        groupB = [];
+
+        function MarkerOptions(group){
+            if(group=="A"){
+                color="red";
+            }else{
+                color="blue";
+            }
+            return {
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8,
+                color:color
+            };
+        } 
+        
+        for(let i=0; i<5; i++){
+            groupA.push(L.circleMarker([51.505, -0.09+(i*0.02)], MarkerOptions("A")).bindPopup(`Marker A${i+1}`));
+            groupB.push(L.circleMarker([51.505+(i*0.02), -0.11], MarkerOptions("B")).bindPopup(`Marker B${i+1}`))
+        }
+        
+        let A = L.layerGroup(groupA);
+        let B = L.layerGroup(groupB);
+        
+        baseMaps={
+            "Street Map":street
+        };
+        
+        overlayMaps={
+            "Group A":A,
+            "Group B":B
+        };
+        
+        map = L.map('map',{
+            center: [51.505, -0.09],
+            zoom: 11,
+            layers: [street, A, B]
+        });
+        
+        layerControl.remove();
+        layerControl = L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(map);
+    }; 
+};
