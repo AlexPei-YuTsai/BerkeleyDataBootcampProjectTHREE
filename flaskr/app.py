6f082687d56119b191f962f865c9e63555dc0b7a
Alex Tsai <alex.tsai45@gmail.com>
2023-08-10 10:47:06 +0800

    did stuff

--- flaskr/app.py
+++ flaskr/app.py
@@ -2,7 +2,7 @@
 import numpy as np
 import datetime as dt
 
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, jsonify
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -13,20 +13,20 @@ from sqlalchemy import create_engine, func
 #################################################
 # Database Setup
 #################################################
-#engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # Reflect an existing database into a new model
-#Base = automap_base()
+Base = automap_base()
 
 # Reflect the tables
-#Base.prepare(autoload_with = engine)
+Base.prepare(autoload_with = engine)
 
 # Save references to each table
-#Measurement = Base.classes.measurement
-#Station = Base.classes.station
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-#session = Session(bind = engine)
+session = Session(bind = engine)
 
 #################################################
 # Flask Setup
@@ -39,7 +39,34 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # List all the available routes.
+
+    # Start SQL session
+    session = Session(bind = engine)
+
+    # Grab everything Station-related
+    query = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation)
+
+    # Make sure to close open sessions
+    session.close()
+
+    # Organize everything into its proper JSON response
+    result_list = []
+    for station, name, latitude, longitude, elevation in query:
+        result_dict={}
+        result_dict["station"]=station
+        result_dict["name"]=name
+        result_dict["latitude"]=latitude
+        result_dict["longitude"]=longitude
+        result_dict["elevation"]=elevation
+        result_list.append(result_dict)
+
+    # Return the JSON representation of your dictionary
+    test = jsonify(result_list)
     return render_template("home.html")
+
+@app.route("/test")
+def test():
+    return jsonify()
 
 @app.errorhandler(404)
 def page_not_found(error):
--- flaskr/static/js/logic.js
+++ flaskr/static/js/logic.js
@@ -1,6 +1,68 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let street = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
-}).addTo(map);
+});
+
+groupA = [];
+groupB = [];
+
+function MarkerOptions(group){
+    if(group=="A"){
+        color="red";
+    }else{
+        color="blue";
+    }
+    return {
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+        color:color
+    };
+} 
+
+for(let i=0; i<5; i++){
+    groupA.push(L.circleMarker([51.505, -0.09+(i*0.02)], MarkerOptions("A")).bindPopup(`Marker A${i+1}`));
+    groupB.push(L.circleMarker([51.505+(i*0.02), -0.11], MarkerOptions("B")).bindPopup(`Marker B${i+1}`))
+}
+
+let A = L.layerGroup(groupA);
+let B = L.layerGroup(groupB);
+
+let baseMaps={
+    "Street Map":street
+};
+
+let overlayMaps={
+    "Group A":A,
+    "Group B":B
+};
+
+let map = L.map('map',{
+    center: [51.505, -0.09],
+    zoom: 11,
+    layers: [street, A, B]
+});
+
+d3.json("/test")
+
+let layerControl = L.control.layers(baseMaps, overlayMaps, {collapsed:false}).addTo(map);
+
+let x = [1,2,3,4,5];
+let y = [5,4,3,2,1];
+let plotData = [{
+    type:"plot",
+    x:x,
+    y:y,
+}];
+// Autorange used to reverse the default ordering of the horizontal bar chart
+let plotLayout = {
+title:`Test`,
+xaxis:{title:"TestX"},
+yaxis:{
+    title:"TestY"
+}
+};
+
+Plotly.newPlot("plot", plotData, plotLayout);
+
+console.log(a)
