37db477d93b8fc2804b9d4bf1e66c3a0f6f392fd
Alex Tsai <alex.tsai45@gmail.com>
2023-08-11 08:14:57 +0800

    minor edit

--- flaskr/database.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib as plt
-from sqlalchemy import create_engine, text
-
-engine = create_engine('sqlite://', echo = False)
-Plastic_df = pd.read_excel("assets/Plastic Data.xlsx")
-
-Plastic_df.to_sql('oceanplastic', con=engine)
